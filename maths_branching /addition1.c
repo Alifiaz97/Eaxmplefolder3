24b2b54666edfc5a34d7426985184d09077d1c0d
Ali <P16170384@KLIXX201WIN007W.lec-admin.dmu.ac.uk>
2018-02-02 14:41:45 +0000

    added 5 user entry.

--- maths_branching /addition1.c
+++ maths_branching /addition1.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
-int addNumbers(int a, int b);         // function prototype
+int addNumbers(int a, int b, int c, int d, int e);         // function prototype
 
 int main()
 {
-    int n1,n2,sum;
+    int n1,n2, n3, n4, n5, sum;
 
     printf("Enters two numbers: ");
-    scanf("%d %d",&n1,&n2);
+    scanf("%d %d %d %d %d",&n1,&n2,&n3,&n4,&n5);
 
-    sum = addNumbers(n1, n2);        // function call
+    sum = addNumbers(n1, n2, n3, n4, n5);        // function call
 
     printf("sum = %d",sum);
 
     return 0;
 }
 
-int addNumbers(int a,int b)         // function definition   
+int addNumbers(int a,int b, int c, int d, int e)         // function definition   
 {
     int result;
-    result = a+b;
+    result = a+b+c+d+e;
     return result;                  // return statement
 }
 
